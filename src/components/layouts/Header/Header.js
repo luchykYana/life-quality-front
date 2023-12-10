49bd88a51b117cd9bee6909dd890baf703a20038
luchykYana <86564110+luchykYana@users.noreply.github.com>
2023-12-10 11:52:10 +0200

    fix: header menu

--- src/components/layouts/Header/Header.js
+++ src/components/layouts/Header/Header.js
@@ -1,18 +1,19 @@
 import {styles} from './styles';
 import {Logout} from '@mui/icons-material';
 import {IconButton} from '@mui/material';
-import {useNavigate} from 'react-router-dom';
+import {useLocation, useNavigate} from 'react-router-dom';
 import {useCallback} from 'react';
 import {useDispatch} from 'react-redux';
 import {authActions} from '../../../store';
 
 const Header = () => {
+    const {pathname} = useLocation();
     const navigate = useNavigate();
     const dispatch = useDispatch()
 
     const logout = useCallback(() => {
         dispatch(authActions.setUserIdZero())
-        navigate('/', { replace: true });
+        navigate('/', {replace: true});
     }, [dispatch, navigate])
 
     return (
@@ -21,17 +22,18 @@ const Header = () => {
                 <img src="/logo_image.png" alt="logo" width={45} height={50}/>
                 <img src="/logo_text.png" alt="logo-text" width={40} height={25}/>
             </div>
-            <div style={styles.items}>
+            {pathname !== '/' && <div style={styles.items}>
                 <div>Clinic name</div>
                 <div>Services</div>
                 <div>Contact</div>
                 <div>About LQ</div>
-            </div>
-            <div>
+            </div>}
+            {pathname !== '/' && <div>
                 <IconButton onClick={logout}>
                     <Logout style={styles.buttonIcon}/>
                 </IconButton>
             </div>
+            }
         </div>
     );
 };
