97c31fd2aa193eda5c5aecadca003d9965cef1a4
luchykYana <86564110+luchykYana@users.noreply.github.com>
2023-12-10 11:41:02 +0200

    feat: header menu

--- src/components/layouts/Header/Header.js
+++ src/components/layouts/Header/Header.js
@@ -1,14 +1,37 @@
 import {styles} from './styles';
+import {Logout} from '@mui/icons-material';
+import {IconButton} from '@mui/material';
+import {useNavigate} from 'react-router-dom';
+import {useCallback} from 'react';
+import {useDispatch} from 'react-redux';
+import {authActions} from '../../../store';
 
 const Header = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch()
+
+    const logout = useCallback(() => {
+        dispatch(authActions.setUserIdZero())
+        navigate('/', { replace: true });
+    }, [dispatch, navigate])
+
     return (
         <div style={styles.header}>
             <div style={styles.logo}>
-                <img src="/logo_image.png" alt="logo-image" width={45} height={50}/>
+                <img src="/logo_image.png" alt="logo" width={45} height={50}/>
                 <img src="/logo_text.png" alt="logo-text" width={40} height={25}/>
             </div>
-            <div></div>
-            <div></div>
+            <div style={styles.items}>
+                <div>Clinic name</div>
+                <div>Services</div>
+                <div>Contact</div>
+                <div>About LQ</div>
+            </div>
+            <div>
+                <IconButton onClick={logout}>
+                    <Logout style={styles.buttonIcon}/>
+                </IconButton>
+            </div>
         </div>
     );
 };
--- src/components/layouts/Header/styles.js
+++ src/components/layouts/Header/styles.js
@@ -5,10 +5,26 @@ export const styles = {
         position: 'fixed',
         top: 0,
         borderBottom: '1px solid #21407E',
-        background: 'white'
+        background: 'white',
+        display: 'flex',
+        alignItems: 'center',
+        color: '#21407E',
+        fontSize: '25px',
+        fontWeight: 'bold'
     },
     logo: {
         width: 'fit-content',
         margin: '1vh 100px'
+    },
+    items: {
+        display: 'flex',
+        width: '60%',
+        justifyContent: 'space-between',
+        padding: '0 40px',
+        marginRight: '100px'
+    },
+    buttonIcon: {
+        color: '#21407E',
+        fontSize: '40px'
     }
 }
