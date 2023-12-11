bec6386c96639b38775a130aead5e346d7168ca9
Oleksandr Shpak <oleksandr.o.shpak@gmail.com>
2023-12-11 12:29:55 +0200

    bugfix

--- src/components/comparisons/SavedResults/DiagramSection/DiagramSection.js
+++ src/components/comparisons/SavedResults/DiagramSection/DiagramSection.js
@@ -1,15 +1,18 @@
 import {styles} from '../../../FilterForm/styles';
 import {FormControl, InputLabel, MenuItem, Select} from '@mui/material';
-import {useEffect, useMemo, useState} from 'react';
+import {useEffect, useMemo, useState, useRef} from 'react';
 import {BarChart} from '@mui/x-charts/BarChart';
 import {PDFDownloadLink} from '@react-pdf/renderer';
 import Button from '@mui/material/Button';
 import {ComparisonPdf} from '../ComparisonPdf';
+import html2canvas from 'html2canvas';
+import ReactBarChart from './ReactBarChart';
 
 const DiagramSection = ({results}) => {
     const [parameter, setParameter] = useState('');
     const [values, setValues] = useState([0])
     const [dates, setDates] = useState([''])
+    const [chartData, setChartData] = useState(null)
 
     const handleChangeParameter = (event) => {
         setParameter(event.target.value);
@@ -40,6 +43,33 @@ const DiagramSection = ({results}) => {
         }
     }, [memoizedQuestionValues, results])
 
+
+    // useEffect(() => {
+    //     console.log('useEffect triggered');
+    //     if (chartRef.current) {
+    //       html2canvas(chartRef.current).then((canvas) => {
+    //         // `canvas` contains the chart as an image
+    //         const dataURL = canvas.toDataURL();
+    //         console.log(dataURL);
+    //         setChartData(dataURL);
+    //         // Now you can use `dataURL` as needed (e.g., display it, save it, etc.)
+    //       });
+    //     }
+    //   }, [values]);
+
+    //   const getChartImage = () => {
+    //     console.log('function triggered');
+    //     if (chartRef.current) {
+    //       html2canvas(chartRef.current).then((canvas) => {
+    //         // `canvas` contains the chart as an image
+    //         const dataURL = canvas.toDataURL();
+    //         console.log(dataURL);
+    //         setChartData(dataURL);
+    //         // Now you can use `dataURL` as needed (e.g., display it, save it, etc.)
+    //       });
+    //     }
+    //   }
+
     return (
         <div style={{display: 'flex', marginTop: '100px'}}>
             <div style={{display: 'flex', flexDirection: 'column', gap: '100px'}}>
@@ -58,12 +88,12 @@ const DiagramSection = ({results}) => {
             </FormControl>}
                 <Button variant={'contained'} style={{height: '40px', background: '#E28B36', marginTop: '28px', width: '80%'}}>
                     <PDFDownloadLink
-                        document={<ComparisonPdf data={{}}/>}
+                        document={<ComparisonPdf data={results} chartData={chartData}/>}
                         fileName="ComparisonResults.pdf"
                         style={{textDecoration: 'none', color: 'white'}}
                     >
                         {({blob, url, loading, error}) =>
-                            loading ? 'Loading result...' : 'Download result'
+                            loading ? 'Loading result...' : 'Download comparison'
                         }
                     </PDFDownloadLink>
                 </Button>
@@ -75,6 +105,15 @@ const DiagramSection = ({results}) => {
                 height={300}
                 colors={['#E28B36']}
             />
+            <div style={{ visibility: 'hidden' }}>
+                <ReactBarChart
+                
+                    data = {results}
+                    field1={dates}
+                    field2={values}
+                    setDiagram={setChartData}
+                />
+            </div>
         </div>
     );
 };
---
+++ src/components/comparisons/SavedResults/DiagramSection/ReactBarChart.js
@@ -0,0 +1,47 @@
+import React, {useEffect, useRef} from 'react';
+import Chart from 'chart.js/auto';
+
+const ReactBarChart = ({data, field1, field2, setDiagram}) => {
+    const chartRef = useRef(null);
+
+    useEffect(() => {
+        const chartCanvas = chartRef.current.getContext('2d');
+
+        if (chartRef.current.chart) {
+            chartRef.current.chart.destroy();
+        }
+
+        const colors = ['rgba(75, 192, 192, 0.6)', 'rgba(255, 99, 132, 0.6)', 'rgba(54, 162, 235, 0.6)']; // Масив кольорів
+        // тут воно шось не то малює, але я хз як то поправити
+        chartRef.current.chart = new Chart(chartCanvas, {
+            type: 'bar',
+            data: {
+                labels: field1,
+                datasets: [
+                    {
+                        label: 'Кількість',
+                        data: field2,
+                        backgroundColor: colors, // Використання масиву кольорів
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    y: {
+                        type: 'linear',
+                        beginAtZero: true,
+                    },
+                },
+                animation: {
+                    onComplete: function () {
+                        setDiagram(this.toBase64Image())
+                    },
+                }
+            },
+        });
+    }, [data]);
+
+    return <canvas ref={chartRef} className={`mb-5`}/>;
+};
+
+export default ReactBarChart;
