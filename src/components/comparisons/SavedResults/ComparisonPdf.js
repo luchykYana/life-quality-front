689b195f7f15ac23840ba7962d7d44d3d0c85e26
Oleksandr Shpak <oleksandr.o.shpak@gmail.com>
2023-12-11 12:29:55 +0200

    bugfix

--- src/components/comparisons/SavedResults/ComparisonPdf.js
+++ src/components/comparisons/SavedResults/ComparisonPdf.js
@@ -55,10 +55,45 @@ const styles = StyleSheet.create({
     }
 });
 
-const ComparisonPdf = ({data}) => {
+const ComparisonPdf = ({data, chartData}) => {
+    console.log(data)
+    console.log(chartData)
     return (
         <Document>
             <Page style={styles.body}>
+                <Text style={styles.title}>Comparison of surveys</Text>
+                    {
+                        // data[0] &&
+                        <View>
+                            <Text>Patient: {data[0]?.patient.firstName + ' ' + data[0]?.patient.lastName}</Text> 
+                            <Text>Age: {new Date(data[0]?.patient.birthDate).toLocaleDateString('en-GB', { day: 'numeric', month: 'numeric', year: 'numeric' })}</Text> 
+                            <Text>Gender: {data[0]?.patient.gender}</Text> 
+                            <Text>Questionnaire: {data[0]?.questionnaire.questionnaireName}</Text> 
+                            
+
+                            {data?.map((result, resultIndex) => (
+                                <View key={resultIndex}>
+                                    <Text>Date: {new Date(result?.date).toLocaleDateString('en-GB', { day: 'numeric', month: 'numeric', year: 'numeric' })}</Text>
+                                    <Text></Text>
+                                    {result.resultsPatientAnswers.map((answer, index) => (
+                                    <View key={index}>
+                                        <Text>
+                                        {index + 1}. {answer.patientAnswer.questionText}
+                                        </Text>
+                                        <Text>
+                                        {answer.patientAnswer.answerText}
+                                        </Text>
+                                    </View>
+                                    ))}
+                                </View>
+                                ))}
+
+                        </View>
+                    }
+            </Page>
+            <Page>
+                    <Image style={styles.image} src={chartData}/>
+                    <Text style={styles.imageDescription}>Comparison of chosen parameters</Text> 
             </Page>
         </Document>
     );
