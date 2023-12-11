aed1616e3d2f8f2f349b974d52c6244f52e142d3
luchykYana <86564110+luchykYana@users.noreply.github.com>
2023-12-11 13:17:13 +0200

    fix(saved)

--- src/components/comparisons/SavedResults/SavedResults.js
+++ src/components/comparisons/SavedResults/SavedResults.js
@@ -1,14 +1,17 @@
 import {useSelector} from 'react-redux';
 import PatientResultsSection from './PatientResultsSection';
 import DiagramSection from './DiagramSection';
+import {Typography} from '@mui/material';
 
 const SavedResults = () => {
     const {savedResults} = useSelector(store => store.surveyReducer)
 
     return (
-        <div>
+        <div>{savedResults && savedResults.length > 0 && <div>
             <PatientResultsSection results={savedResults}/>
             <DiagramSection results={savedResults}/>
+        </div>}
+            {(!savedResults || savedResults.length < 1) && <Typography variant={'h4'} fontWeight={'bold'} marginTop={'30px'}>Nothing for comparison!</Typography>}
         </div>
     );
 };
